38b1040a4854fe38815d612b11cf1e19acd5d987
srollera <srollera@natur.cuni.cz>
2026-04-20 16:01:27 +0200

    first commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 5720058,
+    "boundingBox": {
+        "lx": -645016.71,
+        "ly": -982742.20001,
+        "lz": 1280.8499800000002,
+        "ux": -644952.17,
+        "uy": -982677.6600100001,
+        "uz": 1345.389979999921
+    },
+    "tightBoundingBox": {
+        "lx": -645016.71,
+        "ly": -982742.20001,
+        "lz": 1280.8499800000002,
+        "ux": -644952.17,
+        "uy": -982677.70001,
+        "uz": 1309.0499300000001
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.5589327812194824,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
